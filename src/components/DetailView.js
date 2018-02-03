e0165433ecce860026d4a809b231c5c49038958f
Alex W <alex.w.walton@gmail.com>
2018-02-03 11:47:55 +0000

    Refactoring CSS

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import './DetailView.css';
+
 const DetailView = ({ definition }) => (
   <div className="DetailView">
     <h3>Detail View</h3>
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import './ListView.css';
+
 const ListViewItem = ({ definition, onItemClicked }) => (
   <div className="ListViewItem">
     <h4 onClick={onItemClicked}>
